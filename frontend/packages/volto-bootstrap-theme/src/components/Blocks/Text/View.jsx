997b19ae3ad347f9f32349a6a4af0396987b88a5
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-05-11 16:55:55 +0200

    feat: add a sorrounding div to the slate block view

---
+++ frontend/packages/volto-bootstrap-theme/src/components/Blocks/Text/View.jsx
@@ -0,0 +1,12 @@
+import 'react';
+import TextBlockView from '@plone/volto-slate/blocks/Text/TextBlockView';
+
+const BootstrapTextBlockView = (props) => {
+  return (
+    <div className="block text-block">
+      <TextBlockView {...props} />
+    </div>
+  );
+};
+
+export default BootstrapTextBlockView;
--- frontend/packages/volto-bootstrap-theme/src/config/blocks.js
+++ frontend/packages/volto-bootstrap-theme/src/config/blocks.js
@@ -1,12 +1,15 @@
 import { composeSchema } from '@plone/volto/helpers';
 import { buttonNewProperties } from '@codesyntax/volto-bootstrap-theme/components/Blocks/Button/button_type_schema';
 
+import TextBlockView from '@codesyntax/volto-bootstrap-theme/components/Blocks/Text/View.jsx';
 export default function install(config) {
   //console.log(config.blocks.blocksConfig.__button);
   config.blocks.blocksConfig.__button = {
     ...config.blocks.blocksConfig.__button,
     schemaEnhancer: composeSchema(({ schema }) => buttonNewProperties(schema)),
   };
+
+  config.blocks.blocksConfig.slate.view = TextBlockView;
 
   return config;
 }
