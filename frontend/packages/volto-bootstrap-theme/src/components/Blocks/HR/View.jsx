f65d40289ec31c61677de20d2982c789f79dbc7d
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-07-28 20:18:25 +0200

    s/class/className

--- frontend/packages/volto-bootstrap-theme/src/components/Blocks/HR/View.jsx
+++ frontend/packages/volto-bootstrap-theme/src/components/Blocks/HR/View.jsx
@@ -1,5 +1,5 @@
 const View = (props) => {
-  return <hr class="my-5 border border-primary opacity-100" />;
+  return <hr className="my-5 border border-primary opacity-100" />;
 };
 
 export default View;
