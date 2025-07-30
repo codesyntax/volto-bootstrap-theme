9c270ee9fd0e1e4b5e501bfb44774de5a832ad4f
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-07-30 18:07:13 +0200

    feat: pre- and post-Header slots

--- docs/slots.md
+++ docs/slots.md
@@ -11,3 +11,17 @@ These are the slots provided by this theme:
 - postFooter: all components registered in this slot, will be rendered before the `<footer>` tag, in a div with a class named `post-footer`.
 
 - postFooterContent: all components registered in this slot, will be rendered inside the `<footer>` tag, in a div with a class named `post-footer-content`.
+
+- preHeader: all components registered in this slot, will be rendered before the `<header>` tag, in a div with a class named `pre-header`.
+
+- preHeaderContent: all components registered in this slot, will be rendered inside the `<header>` tag, in a div with a class named `pre-header-content`.
+
+- postHeader: all components registered in this slot, will be rendered before the `<header>` tag, in a div with a class named `post-header`.
+
+- postHeaderContent: all components registered in this slot, will be rendered inside the `<header>` tag, in a div with a class named `post-header-content`.
+
+All those slots receive the current `content` as parameters.
+
+The theme also provides the `aboveContent` and `belowContent` slots that Volto provides by itself.
+belowContent
+Refer to [Volto Slots documentation](https://6.docs.plone.org/volto/configuration/slots.html) to learn how to register and configure your own components in those slots.
--- frontend/packages/volto-bootstrap-theme/src/components/Header/Header.jsx
+++ frontend/packages/volto-bootstrap-theme/src/components/Header/Header.jsx
@@ -1,24 +1,41 @@
+import { useSelector, shallowEqual } from 'react-redux';
 import { Logo } from '@codesyntax/volto-bootstrap-theme/components/Logo/Logo';
 import Navigation from '@codesyntax/volto-bootstrap-theme/components/Navigation/Navigation';
 import SearchLink from '@codesyntax/volto-bootstrap-theme/components/SearchLink/SearchLink';
 import LanguageSelector from '@codesyntax/volto-bootstrap-theme/components/LanguageSelector/LanguageSelector';
 import config from '@plone/registry';
+import SlotRenderer from '@plone/volto/components/theme/SlotRenderer/SlotRenderer';
 
 const Header = ({ pathname }) => {
   const Container = config.getComponent('Container').component;
+  const content = useSelector((state) => state.content?.data, shallowEqual);
+
   return (
     <>
+      <div className="pre-header">
+        <SlotRenderer name="preHeader" content={content} />
+      </div>
+
       <header id="mainheader" className="sticky-lg-top">
         <Container
           id="header-container"
           className="px-2 px-lg-3 d-lg-flex justify-content-lg-between align-items-lg-center"
         >
+          <div className="pre-header-content">
+            <SlotRenderer name="preHeaderContent" content={content} />
+          </div>
           <Logo />
           <Navigation pathname={pathname} />
           <SearchLink pathname={pathname} />
           <LanguageSelector />
+          <div className="post-header-content">
+            <SlotRenderer name="postHeaderContent" content={content} />
+          </div>
         </Container>
       </header>
+      <div className="post-header">
+        <SlotRenderer name="postHeader" content={content} />
+      </div>
     </>
   );
 };
