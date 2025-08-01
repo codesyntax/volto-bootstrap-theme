4d95afeaa5a13a3e1cfa1631c698feacbe59071c
Mikel Larreategi <mlarreategi@codesyntax.com>
2025-08-01 21:41:12 +0200

    feat: styling based on bootstrap Italia

--- frontend/packages/volto-bootstrap-theme/src/customizations/@root/theme.js
+++ frontend/packages/volto-bootstrap-theme/src/customizations/@root/theme.js
@@ -11,5 +11,6 @@
 import '@plone/volto/../theme/themes/pastanaga-cms-ui/extras/cms-ui.semantic.less';
 // import 'semantic-ui-less/semantic.less';
 import '@plone/volto/../theme/themes/pastanaga/extras/extras.less';
+
 import '@codesyntax/volto-bootstrap-theme/theme/main.scss';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
